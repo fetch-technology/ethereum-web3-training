c0486e7cc060e7ab4b6db63406879a6fda884420
Tung Dao <me@tungdao.com>
2018-04-11 11:10:40 +0700

    initial commit

---
+++ brunch-config.js
@@ -0,0 +1,15 @@
+exports.files = {
+  javascripts: {
+    joinTo: 'app.js'
+  },
+
+  stylesheets: {
+    joinTo: 'app.css'
+  }
+}
+
+exports.plugins = {
+  babel: {
+    presets: ['env', 'react']
+  }
+}
